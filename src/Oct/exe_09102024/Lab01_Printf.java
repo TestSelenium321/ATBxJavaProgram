7bea412762c67c3a3cd6de70784d9fb52faf6daa
Balamurali <sbalamurali111@gmail.com>
2024-10-11 11:25:10 +0530

    Oct09Task

--- src/Oct/exe_09102024/Lab01_Printf.java
+++ src/Oct/exe_09102024/Lab01_Printf.java
@@ -7,23 +7,15 @@ public class Lab01_Printf {
         //%d -> int, byte, long, short
         int sample1 = 5 ;
         System.out.printf("%d x %d = %d", sample1, 1, sample1*1);
-        System.out.println();
+        System.out.println(); // print the data in next line
         System.out.printf("%d x %d = %d", sample1, 2, sample1*2);
-        System.out.println();
-        System.out.printf("%d x %d = %d", sample1, 3, sample1*3);
-        System.out.println();
-        System.out.printf("%d x %d = %d", sample1, 4, sample1*4);
-        System.out.println();
 
         byte sample2 = 127;
         System.out.printf("%d x %d = %d", sample2, 1, sample2*1);
         System.out.println();
         System.out.printf("%d x %d = %d", sample2, 2, sample2*2);
         System.out.println();
-        System.out.printf("%d x %d = %d", sample2, 3, sample2*3);
-        System.out.println();
-        System.out.printf("%d x %d = %d", sample2, 4, sample2*4);
-        System.out.println();
+
 
         short sample3   = 32767;
         System.out.printf("%d x %d = %d", sample3, 1, sample2*1);
@@ -32,7 +24,31 @@ public class Lab01_Printf {
         System.out.println();
         System.out.printf("%d x %d = %d", sample3, 3, sample3*3);
         System.out.println();
-        System.out.printf("%d x %d = %d", sample3, 4, sample3*4);
+
+        long sample4 = 767656565L;
+        System.out.printf ("This is long data type %d",sample4);
+
+        //  %s -> String
+        String sample5 = "Myprogram";
+        System.out.printf ("This is string data type %s",sample5);
         System.out.println();
+
+        //%f -> floating, double
+        float sample6 = 3.12f;
+        System.out.printf ("This is float data type %f",sample6);
+        System.out.println();
+
+        double sample7 = 3.2342342342344;
+        System.out.printf("This is double data type %f",sample7);
+
+        //%b -> boolean
+
+        boolean sample8 = true;
+        System.out.printf("This is boolean data type %/b", sample8);
+
+
+
+
+
     }
 }
