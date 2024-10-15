0be7d6e70d3b5d6f9912a7f2b30adae7db5bfcdb
Balamurali <sbalamurali111@gmail.com>
2024-10-15 08:48:03 +0530

    15_Oct Task

---
+++ src/Oct/exe_15102024/Lab01.java
@@ -0,0 +1,26 @@
+package Oct.exe_15102024;
+
+public class Lab01 {
+    public static void main(String[] args) {
+
+        // Largest of Three Numbers a,b,c using the Ternary Operator.
+
+        int a = 30;
+        int b = 70;
+        int c= 100;
+        int result = (a > b) ? (a > c ? a : c) : (b > c ? b : c);
+        System.out.println("The largest number is: " + result);
+
+
+        int e = 10;
+        System.out.println(++e + e++ + e++);
+        // 11 + 11+ 12 = 34
+        // 13
+        System.out.println(e);
+
+        int f = 20;
+        System.out.println(--f + f++ + f--);
+        //19+19+18
+        //19+21+19
+    }
+}
