18d5c339d8170a149b221aa86155db348bfa0898
Balamurali <sbalamurali111@gmail.com>
2024-10-15 07:07:47 +0530

    08_Oct Task

---
+++ src/Oct/exe_08102024/Lab01_DataTypes_RulesIdentifier.java
@@ -0,0 +1,12 @@
+package Oct.exe_08102024;
+
+public class Lab01_DataTypes_RulesIdentifier {
+
+    public static void main(String[] args) {
+        /*
+
+Refer excel sheet in local drive for details "Task 8_Oct"
+
+         */
+    }
+}
