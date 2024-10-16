d607071089b00c8b56af408f39103fa406ec0f6c
Balamurali <sbalamurali111@gmail.com>
2024-10-16 16:51:44 +0530

    11_Oct Task

---
+++ src/Oct/exe_11102024/Lab01_Operaters1.java
@@ -0,0 +1,19 @@
+package Oct.exe_11102024;
+
+public class Lab01_Operaters1 {
+    public static void main(String[] args) {
+        /*
+        operators in Java, practice all of them with the examples
+
+ 1. Arithmetic operators             -->   +  -  *  /   %    (% - modulus)
+ 2. Relational/Comparision Operators -->  >  >= <  <=  != ==  (!= Not equal to), (== Equalto)
+3. Logical operators  --> = && || !
+4. Increment and Decrement operators  -->  ++ --
+5. Assignament operator --> = +=  -=  *= /= %=
+6. Conditional/Ternary Operator --> ?:
+
+         */
+
+
+    }
+}
