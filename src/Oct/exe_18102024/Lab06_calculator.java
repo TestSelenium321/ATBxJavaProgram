9f661ea6e3548802ed5ad362950f4bbb57a068b2
Balamurali <sbalamurali111@gmail.com>
2024-10-19 15:32:12 +0530

    18_Oct Task

--- src/Oct/exe_18102024/Lab06_calculator.java
+++ src/Oct/exe_18102024/Lab06_calculator.java
@@ -8,49 +8,52 @@ public class Lab06_calculator {
         // multiplication, and division, modus based on user input using switch statements.
         // Inputs :   num 1, num 2, +
           // Output :  num1+num2 → print information.
-       //Scanner scanner = new Scanner(System.in);
-       //System.out.println("Enter the calculator operation");
-       //int cal = scanner.nextInt();
 
+
+        //int Num1;
+       // int Num2;
         System.out.println("Program for simple calculator based on user input and switch case");
+        Scanner scanner = new Scanner(System.in);
 
-        Scanner scanner1 = new Scanner(System.in);
         System.out.println("Enter the first number");
-        int Num1 = scanner1.nextInt();
+        int Num1 = scanner.nextInt();
 
-        Scanner scanner2 = new Scanner(System.in);
         System.out.println("Enter the second number");
-        int Num2 = scanner2.nextInt();
+        int Num2 = scanner.nextInt();
 
-        Scanner scanner3 = new Scanner (System.in);
         System.out.println("Enter the Arithmetic operator");
-        char operator = scanner3.next().charAt(0);
+        char operator = scanner.next().charAt(0);
+        //next().charAt(0) to take character type input from keyboard.
+
 
         switch (operator) {
             case '+':
-                System.out.println("Addition");
-                break;
+                int addition = Num1+Num2;
+                System.out.println("The result is addition: " + addition);
+               // OR ..System.out.println("The result is addition: " + "=" + (Num1+Num2));
+               break;
             case '-':
-                System.out.println("Subtraction");
-                break;
+                int Subtraction = Num1-Num2;
+                System.out.println("The result is subtraction: " + "=" + (Num1-Num2));
+               break;
             case '*':
-                System.out.println("Multiplication");
+                int Multiplication = Num1*Num2;
+                System.out.println("The result is multiplication: " + "=" + (Num1*Num2));
                 break;
             case '/':
-                System.out.println("Division");
+                int Division = Num1/Num2;
+                System.out.println("The result is division: " + "=" + (Num1/Num2));
                 break;
             case '%':
-                System.out.println("Modulous");
-                break;
+                int Modulous = Num1%Num2;
+                System.out.println("The result is Modulous: " + "=" + (Num1%Num2));
+               break;
             default:
-                System.out.println("invalid");
-                break;
+                System.out.println("invalid, Please enter valid operator");
+               break;
         }
-       /* scanner1.close();
-        scanner2.close();
-        scanner3.close();*/
+        scanner.close();
 
           }
-
 
 }
